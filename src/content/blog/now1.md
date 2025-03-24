5217bba8f939d87f28f74d78417e2849f2288558
Laker Turner <la@laker.gay>
2025-03-24 18:13:50 +0000

    before push commit

---
+++ src/content/blog/now1.md
@@ -0,0 +1,6 @@
+---
+title: "/now: 2025-W13"
+description: "its now"
+date: 2025-03-24
+tags: now
+---
--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -1,18 +1,25 @@
 import conf from "~/site.config";
-// todo: add blog detection
 
-export const titleConstructor = (title: string) => {
-  console.log(title)
-  if (!title) return conf.siteName;
-  return `${title} | ${conf.siteName}`;
+export const isBlog = (pathname: string) => {
+	return pathname.startsWith("/blog");
+};
+
+export const titleConstructor = (title: string, pathname: string) => {
+	if (isBlog(pathname)) {
+		return title
+			? `${title} | Everything And The Girl`
+			: "Everything And The Girl";
+	}
+	if (!title) return conf.siteName;
+	return `${title} | ${conf.siteName}`;
 };
 
 export const descriptionConstructor = (description: string) => {
-  if (!description) return conf.description;
-  return `${description}`;
+	if (!description) return conf.description;
+	return `${description}`;
 };
 
 export const noteTitleConstructor = (title: string, date: Date) => {
-  if (title) return title;
-  return `A note from ${date.toLocaleDateString()}`;
+	if (title) return title;
+	return `A note from ${date.toLocaleDateString()}`;
 };
