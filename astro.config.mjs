b5e36b3a2154f13b9def1aaceeba50027cebc672
Laker Turner <la@laker.gay>
2025-04-23 22:35:48 +0100

    fly

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import config from './src/site.config'
 import tailwindcss from '@tailwindcss/vite';
 import pagefind from "astro-pagefind";
+
 // https://astro.build/config
 export default defineConfig({
   site: config.site.url,
