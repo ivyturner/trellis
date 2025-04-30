2ff196ff76ceb1c3988023bb89e0b9ad25c7998e
Ivy Turner <ivy@ivy.rs>
2025-04-30 11:32:28 +0100

    fixes

---
+++ src/lib/date.ts
@@ -0,0 +1,26 @@
+
+function checkDate(date: Date | string): Date {
+  if (typeof date === Date)
+}
+
+export function formattedDate(date: Date, showTime: boolean): string {
+    const options: Intl.DateTimeFormatOptions = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    };
+
+    if (showTime) {
+      options.hour = "2-digit";
+      options.minute = "2-digit";
+    }
+
+    return date.toLocaleString("en-GB", options);
+}
+
+export function isoDate(date: Date): string {
+    if (isNaN(date.getTime())) {
+        throw new Error("Invalid Date");
+    }
+    return date.toISOString();
+}
