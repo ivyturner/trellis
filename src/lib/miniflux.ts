6e8d46e471809ced31e4394ef18d2300d48ef109
Ivy Turner <ivy@ivy.rs>
2025-05-06 13:09:32 +0100

    h

---
+++ src/lib/miniflux.ts
@@ -0,0 +1,19 @@
+async function minifluxAPIRequest(route: string): Promise<Response> {
+  const req = await fetch(`https://rss.houseplants.cloud/v1/${route}`, {
+    headers: {
+      "X-Auth-Token": import.meta.env.MINIFLUX_KEY,
+    },
+  });
+  return req
+}
+
+export async function getMinifluxFeedCount(): Promise<number> {
+  const feeds = await minifluxAPIRequest("feeds")
+
+  const feedData = await feeds.json();
+  return feedData.length;
+}
+
+export async function getFeedsByCategory(id: number): Promise<Object> {
+  
+}
